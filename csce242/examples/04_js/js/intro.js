971797db2572c3810a6da75ae01e8a6da4722853
cmhanner <chanceforcode@gmail.com>
2025-09-11 11:46:31 -0400

    Examples from 9/9

---
+++ csce242/examples/04_js/js/intro.js
@@ -0,0 +1,14 @@
+//Console.log() displays the parameter
+//Three Variables of JC
+// var, let, const
+// Const by default, and let if need be if variable changes
+// Cant use the word function in work
+// const  sayHello = () => {
+//     console.log("Hello World");
+// }
+// document.getElementById("btn-click-me").onclick = sayHello; //  dont put parenthesies
+
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("hellp world"); 
+}
