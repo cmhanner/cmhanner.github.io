3b6c3374f611f1946f0d51966a9d119949ed0a52
cmhanner <chanceforcode@gmail.com>
2025-09-15 18:21:01 -0400

    Sunny times: clicks, it just expands

---
+++ csce242/assignments/05html/interact.js
@@ -0,0 +1,7 @@
+document.getElementById("sunny-times-button").onclick = (event) => {
+    document.getElementById("p-Here-comes").innerHTML = "Here comes the sun"
+    document.getElementById("p-Sun").innerHTML = "Sun"
+    document.getElementById("p-Sun1").innerHTML = "Sun"
+    document.getElementById("p-Sun2").innerHTML = "Sun"
+    document.getElementById("p-Here-it").innerHTML = "Here it comes"
+}
