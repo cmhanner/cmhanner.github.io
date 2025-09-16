abccbb55af633000f928026babeda9262f45e62d
cmhanner <chanceforcode@gmail.com>
2025-09-15 20:23:29 -0400

    Got Select a Color to work

--- csce242/assignments/05html/interact.js
+++ csce242/assignments/05html/interact.js
@@ -6,3 +6,20 @@ document.getElementById("sunny-times-button").onclick = (event) => {
     document.getElementById("p-Sun2").innerHTML = "Sun"
     document.getElementById("p-Here-it").innerHTML = "Here it comes"
 }
+
+const textTocolor = document.getElementById("coloring-text");
+const colorInput = document.getElementById ("pickcolor");
+const hexColor = document.getElementById("hex-color");
+
+// Intialized text color and hex code
+textTocolor.style.color = colorInput.value;
+hexColor.textCotnent = colorInput.value;
+
+
+//  Uppdates when user picks a new color
+colorInput.addEventListener ( "input", () => {
+    textTocolor.style.color = colorInput.value; //  text changes color
+    hexColor.textContent = colorInput.value; //  shows hex code
+    // console.log("Picked Color:" ,colorInput.value);
+
+});
