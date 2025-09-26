ec4690d0e41f11e75e979b023b9a60629c236ed1
cmhanner <chanceforcode@gmail.com>
2025-09-25 20:27:11 -0400

    moved html shadow, leaves, and trunk to css to overlap the clouds and space the trees

--- csce242/assignments/07html/07.js
+++ csce242/assignments/07html/07.js
@@ -2,17 +2,36 @@
 
 const drawButton = document.getElementById("draw-scene");
 const scene = document.querySelector(".scene"); //  sun, moon, clouds, trees
-const cloudTree = document.getElementById("cloud-tree"); // clouds and trees container which to duplicate
+//const cloudTree = document.getElementById("cloud-tree"); // clouds and trees container which to duplicate
+
+const cloudsRow = document.querySelector(".clouds-in-row");
+const treeRow = document.querySelector(".trees-in-row");
+
+//  Check time of day
+
 
 drawButton.addEventListener("click", () => {
-    scene.style.display = "flex";
-    const repeat = 5;
+    scene.style.display = "flex"; //  show the scene
+    
+
+    const repeat = 6;
     for (let i = 0; i < repeat; i++) {
-        const clone = cloudTree.cloneNode(true); // clones the tree and cloud to repeathttps://www.w3schools.com/jsref/met_node_clonenode.asp
-        scene.appendChild(clone);  //  adds the clone to scene
-        scene.style.flexDirection = "row";
-        scene.style.justifyContent = "space-around";
-        scene.style.right = "50px";
+        //  Clouuds
+        const cloud = document.createElement("div"); //  creating new div to put each cloud in
+        cloud.classList.add("cloud");  //  puts class of cloud in each new div
+        cloud.innerHTML = "<span class='shadow'></span>"; //  <!--https://codepen.io/jeyakarthika/pen/AappZV-->
+
+        //  if index = 0, first cloud, no overlap stays in place, else move left 80px
+        cloud.style.marginLeft = i === 0 ? "0" : "-90px";
+        cloudsRow.appendChild(cloud); //  add the cloud to the clouds row
+
+        //Trees
+        const tree = document.createElement("div");
+        tree.classList.add("tree");
+        tree.innerHTML = "<div class='leaves'></div><div class='trunk'></div>"; //  <!--https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_shapes_circle-->
+        tree.style.margin = "0 20px"; //  space the trees out
+        treeRow.appendChild(tree); //  add the tree to the trees row
+        
     }
 
 })
