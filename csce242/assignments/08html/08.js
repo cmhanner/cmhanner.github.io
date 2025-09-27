f9fc8fd201c76e5a7b53b6190214c653833d183c
cmhanner <chanceforcode@gmail.com>
2025-09-27 01:22:52 -0400

    Trying to figure out array for the dogs

--- csce242/assignments/08html/08.js
+++ csce242/assignments/08html/08.js
@@ -1,2 +1,22 @@
 //  Assignment 8: Arrays
 
+window.onload = function () {
+    const beforeImages = [];
+    beforeImages["apollo"] = "images/apollo-before.jpg";
+    beforeImages["harper"] = "images/harper-before.jpg";
+    beforeImages["bow-tie"] = "images/bow-tie-before.jpg";
+    beforeImages["toby"] = "images/toby-before.jpg";
+
+    const imgSpot = document.querySelectorAll('.image-spot'); // Grabs all the image spots 
+
+    beforeImages.forEach((spot, i) => {
+        const img = document.createElement("img");
+        img.src = spot;
+        img.alt = "Before Adoption";
+        imgSpot[i].appendChild(img);
+
+
+    });
+
+
+};
