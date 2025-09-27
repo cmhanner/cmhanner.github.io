980456c3b929215aa9dbdb76a8a1fb10848faef9
cmhanner <chanceforcode@gmail.com>
2025-09-27 15:55:11 -0400

    Img populate from array,need to figure out pop-ups

--- csce242/assignments/08html/08.js
+++ csce242/assignments/08html/08.js
@@ -1,22 +1,45 @@
 //  Assignment 8: Arrays
 
-window.onload = function () {
-    const beforeImages = [];
-    beforeImages["apollo"] = "images/apollo-before.jpg";
-    beforeImages["harper"] = "images/harper-before.jpg";
-    beforeImages["bow-tie"] = "images/bow-tie-before.jpg";
-    beforeImages["toby"] = "images/toby-before.jpg";
-
+window.onload = function () {  //  https://stackoverflow.com/questions/520812/how-do-i-detect-when-a-web-page-is-loaded
+    const beforeImages = [
+        { name: "apollo", src: "img-before/apollo-before.jpg" },
+        { name: "bow-tie", src: "img-before/bow-tie-before.jpg" },
+        { name: "harper", src: "img-before/harper-before.jpg" },
+        { name: "toby", src: "img-before/toby-before.jpg" }
+    ];
     const imgSpot = document.querySelectorAll('.image-spot'); // Grabs all the image spots 
 
-    beforeImages.forEach((spot, i) => {
-        const img = document.createElement("img");
-        img.src = spot;
-        img.alt = "Before Adoption";
-        imgSpot[i].appendChild(img);
+    //  item and index
+    beforeImages.forEach((dog, i) => { 
 
-
+        const img = document.createElement('img'); // Create an img element
+        img.src = dog.src; // Set the source of the image
+        img.alt = "Before Adoption: " + dog.name;
+        img.title = "Please adopt " + dog.name;
+        imgSpot[i].appendChild(img); // Append the image to the corresponding spot
     });
 
+    
 
 };
+
+//  Changes images on click
+
+ const afterImages = [
+        { name: "apollo", src: "img-after/apollo-after.jpg" },
+        { name: "bow-tie", src: "img-after/bow-tie-after.jpg" },
+        { name: "harper", src: "img-after/harper-after.jpg" },
+        { name: "toby", src: "img-after/toby-after.jpg" }
+    ];
+
+    img.addeventListener('click', () => {
+        afterImages.forEach((dog, i) => {
+            document.createElement('img'); // Create an img element
+            img.src = dog.src; // Set the source of the image
+            img.alt = "After Adoption: " + dog.name;
+            img.title =  dog.name + " after adoption";
+            imgSpot[i].appendChild(img); // Append the image to the corresponding spot
+
+        });
+
+    });
