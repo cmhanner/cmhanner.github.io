81f7a001a6a98d12dab4b9e316248be1bb803ca9
cmhanner <chanceforcode@gmail.com>
2025-09-27 16:36:38 -0400

    a popup pops up to the right blank, won't x out

--- csce242/assignments/08html/08.js
+++ csce242/assignments/08html/08.js
@@ -7,7 +7,20 @@ window.onload = function () {  //  https://stackoverflow.com/questions/520812/ho
         { name: "harper", src: "img-before/harper-before.jpg" },
         { name: "toby", src: "img-before/toby-before.jpg" }
     ];
+
+    const afterImages = [
+        { name: "apollo", src: "img-after/apollo-after.jpg" },
+        { name: "bow-tie", src: "img-after/bow-tie-after.jpg" },
+        { name: "harper", src: "img-after/harper-after.jpg" },
+        { name: "toby", src: "img-after/toby-after.jpg" }
+    ];
+
     const imgSpot = document.querySelectorAll('.image-spot'); // Grabs all the image spots 
+    const popUp = document.getElementById('popup'); // Grabs the popup element
+    const popUpTitle = document.getElementById('popup-title'); // Grabs the popup title element
+    const popUpImg = document.getElementById('popup-img'); // Grabs the popup image element
+    const popUpClose = document.getElementById('close'); // Grabs the popup close button
+
 
     //  item and index
     beforeImages.forEach((dog, i) => { 
@@ -17,29 +30,28 @@ window.onload = function () {  //  https://stackoverflow.com/questions/520812/ho
         img.alt = "Before Adoption: " + dog.name;
         img.title = "Please adopt " + dog.name;
         imgSpot[i].appendChild(img); // Append the image to the corresponding spot
+
+        img.addEventListener('click', () => {
+            console.log(dog.name + " clicked");
+            popUpTitle.textContent = dog.name + " after adoption"; // Set the popup title
+            popUpImg.src = afterImages[i].src;
+            popUp.classList.remove('hidden'); // Show the popup
+
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popUp.classList.add('hidden'); // Hide the popup
+        });
+        
     });
 
+    
     
 
 };
 
 //  Changes images on click
 
- const afterImages = [
-        { name: "apollo", src: "img-after/apollo-after.jpg" },
-        { name: "bow-tie", src: "img-after/bow-tie-after.jpg" },
-        { name: "harper", src: "img-after/harper-after.jpg" },
-        { name: "toby", src: "img-after/toby-after.jpg" }
-    ];
+ 
 
-    img.addeventListener('click', () => {
-        afterImages.forEach((dog, i) => {
-            document.createElement('img'); // Create an img element
-            img.src = dog.src; // Set the source of the image
-            img.alt = "After Adoption: " + dog.name;
-            img.title =  dog.name + " after adoption";
-            imgSpot[i].appendChild(img); // Append the image to the corresponding spot
-
-        });
-
-    });
+    
