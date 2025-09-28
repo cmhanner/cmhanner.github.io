67c7fa4773824740f982f1f84950c8cdda5e2324
cmhanner <chanceforcode@gmail.com>
2025-09-27 22:12:24 -0400

    got before picture hover,  assignment up on main

--- csce242/assignments/08html/08.js
+++ csce242/assignments/08html/08.js
@@ -2,47 +2,56 @@
 
 window.onload = function () {  //  https://stackoverflow.com/questions/520812/how-do-i-detect-when-a-web-page-is-loaded
     const beforeImages = [
-        { name: "apollo", src: "img-before/apollo-before.jpg" },
-        { name: "bow-tie", src: "img-before/bow-tie-before.jpg" },
-        { name: "harper", src: "img-before/harper-before.jpg" },
-        { name: "toby", src: "img-before/toby-before.jpg" }
+        { name: "Apollo", src: "img-before/apollo-before.jpg" },
+        { name: "Bow-Tie", src: "img-before/bow-tie-before.jpg" },
+        { name: "Harper", src: "img-before/harper-before.jpg" },
+        { name: "Toby", src: "img-before/toby-before.jpg" }
     ];
 
     const afterImages = [
-        { name: "apollo", src: "img-after/apollo-after.jpg" },
-        { name: "bow-tie", src: "img-after/bow-tie-after.jpg" },
-        { name: "harper", src: "img-after/harper-after.jpg" },
-        { name: "toby", src: "img-after/toby-after.jpg" }
+        { name: "Apollo", src: "img-after/apollo-after.jpg" },
+        { name: "Bow-tie", src: "img-after/bow-tie-after.jpg" },
+        { name: "Harper", src: "img-after/harper-after.jpg" },
+        { name: "Toby", src: "img-after/toby-after.jpg" }
     ];
 
-    const imgSpot = document.querySelectorAll('.image-spot'); // Grabs all the image spots 
-    const popUp = document.getElementById('popup'); // Grabs the popup element
-    const popUpTitle = document.getElementById('popup-title'); // Grabs the popup title element
-    const popUpImg = document.getElementById('popup-image'); // Grabs the popup image element
-    const popUpClose = document.getElementById('close'); // Grabs the popup close button
-    const imgHover = document.getElementById('dog-hover');
+    const imgSpot = document.querySelectorAll('.image-spot'); //  image spots 
+    const popUp = document.getElementById('popup'); // popup element
+    const popUpTitle = document.getElementById('popup-title'); // popup title element
+    const popUpImg = document.getElementById('popup-image'); // popup image element
+    const popUpClose = document.getElementById('close'); // popup close button
+
 
 
     //  Load before images into their spots
     //  item and index
     beforeImages.forEach((dog, i) => { 
 
+        //  Wrapper Idea: https://codingislove.com/image-caption-hover-effect/
+        const wrapper = document.createElement('div'); //  create a wrapper div to add the caption
+        wrapper.classList.add('image-wrapper'); //  add class to wrapper
+
         const img = document.createElement('img'); // Create an img element
         img.src = dog.src; // Set the source of the image
         img.alt = "Before Adoption: " + dog.name;
-        imgHover.textContent = "Please adopt " + dog.name;
-        // img.title = "Please adopt " + dog.name;
-        imgSpot[i].appendChild(img); // Append the image to the corresponding spot
+       
+        const caption = this.document.createElement('span'); // Create a span for caption
+        caption.classList.add('caption');
+        caption.textContent = "Please adopt " + dog.name; // Set caption text
+
+        wrapper.appendChild(img); // Append the image to the wrapper
+        wrapper.appendChild(caption); // Append the caption to the wrapper
+        imgSpot[i].appendChild(wrapper); // Append the wrapper containing image and caption to image spots
 
         //  On click show after image
         img.addEventListener('click', (e) => { //  adding a e to get image info when clicked for popup
             console.log(dog.name + " clicked");
             popUpImg.src = afterImages[i].src;
             popUpTitle.textContent = dog.name + " after adoption"; // Set the popup title
-            popUp.classList.remove('hidden'); // Show the 
+            popUp.classList.remove('hidden'); // Show the popup
             
              //  target gets the specific image clicked
-            const popUpposition = e.target.getBoundingClientRect(); // Get the position of the clicked image
+            const popUpposition = e.target.getBoundingClientRect(); // Get the position and size of the clicked image https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
             popUp.style.top = (popUpposition.top + window.scrollY) + 'px'; // Position the popup vertically
             popUp.style.left = (popUpposition.left + window.scrollX) + 'px'; // Position the popup horizontally
 
@@ -63,8 +72,5 @@ window.onload = function () {  //  https://stackoverflow.com/questions/520812/ho
 
 };
 
-//  Changes images on click
 
- 
-
-    
+  
