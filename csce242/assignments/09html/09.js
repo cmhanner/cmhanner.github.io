b9e85cdde493f2279b7695a0ef559d190934a0c6
cmhanner <chanceforcode@gmail.com>
2025-10-02 19:12:54 -0400

    trying to get pictures and title to display

--- csce242/assignments/09html/09.js
+++ csce242/assignments/09html/09.js
@@ -10,21 +10,41 @@ class Art {
 
     //  returns only the title and the image for the object
     get art() {
-        //  the modal
-        const modalContent = document.querySelector("w3-modal-content");
+        //  the modal that contains the section
+        // const modalContent = document.querySelector(".w3-modal-content");
         const section = document.createElement("section");
-        modalContent.append(section); //  adds the section to the modal content
-
+        section.classList.add("art");
         //  name
         const h3 = document.createElement("h3");
-        section.append("h3")
+        h3.textContent = this.name; 
+        section.append(h3);
+
+        //  image
+        const img = this.artImage(this.image);
+        section.append(img);
+
+        return section;
 
     
     }
 
-    
+    artImage (filename) {
+        const img = document.createElement("img");
+        img.src = `images/${filename}`;
+        return img;
 
+    }
+
+    
 } //  class for art
 
+const art = [];
+art.push(new Art("X-Men Wolverine Incredible Hulk 340", "Alex Ross", "wolverine.webp", true));
+art.push(new Art("Spiderman Torment", "Alex Ross", "spiderman.webp", false));
+art.push(new Art("Superman More Powerful", "Alex Ross", "Superman.webp", false))
+art.push(new Art("Avengers 700", "Alex Ross", "avengers+700.webp", false));
 
+//  on page load
+ const artList = document.getElementById("art-list");
+ art.forEach( a => artList.append(a.item));
 
