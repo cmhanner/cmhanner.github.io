8dfad5a96175c18313c35cdab8ad4d53c52f91ac
cmhanner <chanceforcode@gmail.com>
2025-10-02 19:17:34 -0400

    image and title displays

--- csce242/assignments/09html/09.js
+++ csce242/assignments/09html/09.js
@@ -9,11 +9,11 @@ class Art {
     } //  constructor
 
     //  returns only the title and the image for the object
-    get art() {
+    get item() {
         //  the modal that contains the section
         // const modalContent = document.querySelector(".w3-modal-content");
         const section = document.createElement("section");
-        section.classList.add("art");
+        section.classList.add("art-card");
         //  name
         const h3 = document.createElement("h3");
         h3.textContent = this.name; 
@@ -42,9 +42,9 @@ const art = [];
 art.push(new Art("X-Men Wolverine Incredible Hulk 340", "Alex Ross", "wolverine.webp", true));
 art.push(new Art("Spiderman Torment", "Alex Ross", "spiderman.webp", false));
 art.push(new Art("Superman More Powerful", "Alex Ross", "Superman.webp", false))
-art.push(new Art("Avengers 700", "Alex Ross", "avengers+700.webp", false));
+art.push(new Art("Avengers 700", "Alex Ross", "avengers-700.webp", false));
 
 //  on page load
  const artList = document.getElementById("art-list");
- art.forEach( a => artList.append(a.item));
+ art.forEach( a => artList.append(a.item)); 
 
