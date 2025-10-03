6bd6ae7268d233dfa779a6c5e6e842367f732d34
cmhanner <chanceforcode@gmail.com>
2025-10-02 21:39:30 -0400

    close button work, got to clear modal when closses

--- csce242/assignments/09html/09.js
+++ csce242/assignments/09html/09.js
@@ -15,9 +15,9 @@ class Art {
         const section = document.createElement("section");
         section.classList.add("art-card");
         //  name
-        const h3 = document.createElement("h3");
-        h3.textContent = this.name; 
-        section.append(h3);
+        const h2 = document.createElement("h2");
+        h2.textContent = this.name; 
+        section.append(h2);
 
         //  image
         const img = this.artImage(this.image);
@@ -38,16 +38,23 @@ class Art {
 
     }
 
+    description () {
+        const p = document.createElement("p");
+        p.textContent = `By: ${this.artist}`;
+        return p;
+    }
+
     
 
     
 } //  class for art
 
 const art = [];
-art.push(new Art("X-Men Wolverine Incredible Hulk 340", "Alex Ross", "wolverine.webp", true));
+art.push(new Art("X-Men Wolverine Hulk 340", "Alex Ross", "wolverine.webp", true));
 art.push(new Art("Spiderman Torment", "Alex Ross", "spiderman.webp", false));
 art.push(new Art("Superman More Powerful", "Alex Ross", "Superman.webp", false))
 art.push(new Art("Avengers 700", "Alex Ross", "avengers-700.webp", false));
+art.push(new Art("Batman Tribute NYCC", "Alex Ross", "batman.webp", false));
 
 //  on page load
  const artList = document.getElementById("art-list");
@@ -56,15 +63,29 @@ art.push(new Art("Avengers 700", "Alex Ross", "avengers-700.webp", false));
  function ArtModal (artObj) {
     const modal = document.getElementById("modal-id");
     const modalContent = document.querySelector(".w3-modal-content");
+    const closeButton = document.getElementById("modal-close");
 
+   
+    // modalContent.innerHTML = "";
     const h3 = document.createElement("h3");
     h3.textContent = artObj.name;
     modalContent.append(h3);
 
+    
+    modalContent.append(artObj.description());
+
     const img = artObj.artImage(artObj.image);
     modalContent.append(img);
 
+    
+
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+    
+    
+
     modal.style.display = "block";
 
-}
+    }
 
