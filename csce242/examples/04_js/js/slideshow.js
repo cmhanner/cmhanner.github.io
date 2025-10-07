3053d31bdc09ed7b9ef2b404bb4abde2fd764643
cmhanner <chanceforcode@gmail.com>
2025-10-07 12:12:43 -0400

    Class examples Slideshow functionality Added slideshow to JSONS

--- csce242/examples/04_js/js/slideshow.js
+++ csce242/examples/04_js/js/slideshow.js
@@ -1,4 +1,18 @@
 //  function happens every second 
+//  I want the child  of the div that's not class of hidden
 setInterval(() =>{
-    console.log("hi");
+    // const slideshow = document.getElementById("slideshow");
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    let nextSlide = currentSlide.nextElementSibling;
+
+    if (!nextSlide) { //  if next slide equals null
+        nextSlide = document.querySelector("#slideshow :first-child");
+
+    }
+
+    currentSlide.classList.add("hidden");
+    nextSlide.classList.remove("hidden");
+
+    // console.log(currentSlide);
+    //for ()
 }, 1000); //  takes two parameters, first one is the function you call and the second one how often
