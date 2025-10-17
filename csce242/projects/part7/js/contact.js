1f7d9ac05e42c31ff0fefeb36417cc5c6ccb9fe8
cmhanner <chanceforcode@gmail.com>
2025-10-16 20:07:42 -0400

    Per Part 7 Got the Form to Work Got to style Map and Form

---
+++ csce242/projects/part7/js/contact.js
@@ -0,0 +1,48 @@
+const contactForm = document.getElementById("contact-form");
+const result = document.getElementById('contact-result');
+
+if (!contactForm) {
+    console.error("No Contact-Form found on page");
+} else {
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //  prevent user from leaving page
+
+        //  Build JSON body for Web3Forms
+        const formData = new FormData(event.currentTarget);
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+        result.innerHTML = "Please wait..."
+
+        fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+            .then(async (response) => {
+                let json = await response.json();
+                    if (response.status == 200) {
+                        result.innerHTML = "Email Sent";
+                    } else {
+                        console.log(response);
+                        result.innerHTML = json.message;
+                    }
+            })
+                .catch(error => {
+                    console.log(error);
+                    result.innerHTML = "Something went wrong!";
+                })
+                .then(function() {
+                    contactForm.reset();
+                    setTimeout(() => {
+                        result.style.display = "none";
+                    }, 3000);
+                });
+    });
+}
+    
+
+    
+    
